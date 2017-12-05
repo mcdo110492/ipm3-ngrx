ab2f5a7a1835d2b866845ffad74b484a26e6a8a9
fuentebellamcdonald@gmail.com <fuentebellamcdonald@gmail.com>
2017-12-06 01:01:50 +0800

    Remove some other features HRIS Features Only

--- src/app/main-content/sidenav/sidenav.metadata.ts
+++ src/app/main-content/sidenav/sidenav.metadata.ts
@@ -13,16 +13,16 @@ export const superAdminLinks : RouterLinks[]  = [
     { title : 'Positions' , link : 'positions', icon : 'table', isHeader : false },
     { title : 'Employment Status' , link : 'employment/status', icon : 'table', isHeader : false },
     { title : 'Employee Status' , link : 'employee/status', icon : 'table', isHeader : false },
-    { title : 'Units' , link : 'units', icon : 'table', isHeader : false },
-    { title : 'Collection Schedules' , link : 'collection/schedules', icon : 'table', isHeader : false },
-    { title : 'Collection Types' , link : 'collection/types', icon : 'table', isHeader : false },
+    // { title : 'Units' , link : 'units', icon : 'table', isHeader : false },
+    // { title : 'Collection Schedules' , link : 'collection/schedules', icon : 'table', isHeader : false },
+    // { title : 'Collection Types' , link : 'collection/types', icon : 'table', isHeader : false },
     { title : 'Human Resources' , isHeader : true},
     { title : 'Register Employee' , link : 'employee/register', icon : 'book', isHeader : false },
     { title : 'Employee List' , link : 'employee/list', icon : 'people', isHeader : false },
-    { title : 'Equipments' , isHeader : true},
-    { title : 'Equipments' , link : 'equipments', icon : 'directions_bus', isHeader : false },
-    { title : 'GPS' , isHeader : true},
-    { title : 'Shifts' , link : 'shifts', icon : 'place', isHeader : false },
+    // { title : 'Equipments' , isHeader : true},
+    // { title : 'Equipments' , link : 'equipments', icon : 'directions_bus', isHeader : false },
+    // { title : 'GPS' , isHeader : true},
+    // { title : 'Shifts' , link : 'shifts', icon : 'place', isHeader : false },
 ];
 
 
